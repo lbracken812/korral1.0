12d8c3c787fdedd0c758aba59384a70d2da32945
Logan Bracken <82625913+lbracken812@users.noreply.github.com>
2021-10-22 09:40:31 -0500

    (C)

--- components/core/Footer.vue
+++ components/core/Footer.vue
@@ -3,6 +3,9 @@
     <p class="text-xs text-white opacity-50">
       © korral.me {{ year }}
     </p>
+    <p class="text-xs text-white opacity-50">
+     IP by https://rxs.to/
+    </p>
   </div>
 </template>
 
