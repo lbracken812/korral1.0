08196f2ef64fc330e83fbd960db3348c86455424
Logan Bracken <82625913+lbracken812@users.noreply.github.com>
2021-10-21 23:23:46 -0500

    nope

--- pages/index.vue
+++ pages/index.vue
@@ -5,12 +5,3 @@
     <landing-projects />
   </div>
 </template>
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-E7S5QZ694L"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-E7S5QZ694L');
-</script>
